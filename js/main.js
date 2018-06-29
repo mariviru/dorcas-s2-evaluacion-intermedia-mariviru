98b712058ea6e96b4d4294d4da0742fc41b14a2d
Mariví <marivirh82@gmail.com>
2018-06-29 18:33:07 +0200

    add showNumber function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-console.log('>> Ready :)');
+var aleatoryNumber = getRandomNumber;
+var button = document.querySelector('.button');
+var input = document.querySelector('.input');
+var userNumber = input.value;
+
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+
+console.log('> ' + getRandomNumber(100));
+
+
+function showNumber() {
+	userNumber = input.value;
+	if(userNumber > getRandomNumber) {
+		return
+	}
+	console.log(userNumber);
+	
+}
+
+button.addEventListener('click', showNumber);
